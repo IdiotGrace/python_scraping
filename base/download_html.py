e5045d7214d880f7377fe846036ac087ee80d300
haowenchao <haowenchao123@gmail.com>
2019-08-23 14:43:56 +0800

    chp 2

---
+++ base/download_html.py
@@ -0,0 +1,20 @@
+import urllib.request
+from urllib.error import URLError, HTTPError, ContentTooShortError
+
+def Download(url, num_retries=2):
+	print('Downloading:', url)
+	try:
+		html = urllib.request.urlopen(url).read()
+		request.add_header('User-agent', user_agent)
+	except(URLError, HTTPError, ContentTooShortError) as e:
+		print('Downloading error:', e.reason)
+		html = None
+		if num_retries > 0:
+			if hasattr(e, 'code') and 500 <= e.code < 600:
+				# recursively retry 5xx HTTP errors
+				return Download(url, num_retries - 1)
+	return html
+
+
+if __name__ == '__main__':
+	Download('http://httpstat.us/500')
